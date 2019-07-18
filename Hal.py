49dfc17ed3225281a9c399bbd53d0c7ab5895618
Carson <46657662+Darkarea51@users.noreply.github.com>
2019-07-17 19:15:44 -0700

    Update Hal.py

--- Hal.py
+++ Hal.py
@@ -25,13 +25,22 @@ ALLOWED_ID=["322490168034590732","289920025077219328","305845952986480650","2856
 LAST_VIDEO=None
 Meeting_Room=None
 
+
 client=discord.Client()
 #photos(1)=["/home/pi/Desktop/20190119163521_1.JPG","/home/pi/Desktop/20190119162904_1.jpg","/home/pi/Desktop/20190119153640_1.jpg","/home/pi/Desktop/20190119163119_1.jpg","/home/pi/Desktop/20190119162922_1.jpg","/home/pi/Desktop/2019011918210350_1.jpg","/home/pi/Desktop/20190119163119_1.jpg","/home/pi/Desktop/20190119162640_1.jpg","/home/pi/Desktop/20190119161440_1.jpg","/home/pi/Desktop/2019011811719_1.jpg","/home/pi/Desktop/20190119163114_1.jpg","/home/pi/Desktop/20190119143642_1.jpg","/home/pi/Desktop/20170507152646_1.jpg","/home/pi/Desktop/20190120171108_1.jpg","/home/pi/Desktop/20190119162035_1.jpg","/home/pi/Desktop/20190119133028_1.jpg","/home/pi/Desktop/20190119163803_1.jpg","/home/pi/Desktop/20190119162823_1.jpg","/home/pi/Desktop/20190119155705_1.jpg"]     
+commands = []
+command = []
+
+                
+                    
+
+
 Player=None
 Memberinfo=[]
 Blocked=[]
 Voice=[]
-#Months
+
+
 Months = {1: "January",
 2: "Feburary",
 3: "March",
@@ -44,6 +53,9 @@ Months = {1: "January",
 10: "October",
 11: "November",
 12: "December"}
+
+
+
 #Discord Bot Stat (streaming)
 @client.event
 async def on_ready():
@@ -94,19 +106,13 @@ async def on_message(message):
     if str(message.content).upper()=='*CODE':
         em = discord.Embed(colour=3447003)
         em.set_author(name="Github Link: https://bit.ly/2QHtYal")
-        await client.send_message(message.channel, embed=em)
-    if str(message.content).upper()=="*CURSEDIMAGE":
-        await client.send_file(message.channel,r"/home/pi/Desktop/IMG_1679.JPG")
+        await client.send_message(message.channel, embed=em) 
     if str(message.content).upper()=="*THRONEMAP":
         await client.send_file(message.channel,r"/home/pi/Desktop/Photos/Map.png")
-    if str(message.content).upper()=="*SOCIALISM":
-        await client.send_file(message.channel,r"//home/pi/Desktop/IMG_6969.JPG")
     if str(message.content).upper()=="*BIRTHDAY":
         em = discord.Embed(colour=3447003)
         em.set_author(name="January,9th")
         await client.send_message(message.channel, embed=em)
-    if str(message.content).upper()=="*INTERSTELLAR":
-        await client.send_file(message.channel,r"C:\Users\cmwol\Desktop\interstellar_poster_0.JPG")
     if str(message.content).upper()=="*DONATION":
         em = discord.Embed(colour=3447003)
         em.set_author(name="If you want to support MD financially here is a link to his donation page: https://streamlabs.com/MDLive_ ")
@@ -136,12 +142,9 @@ async def on_message(message):
             for i in range (total):
                 await client.send_message(message.channel, "!Mine")
             await client.send_message(message.channel, "!Transfer|{0}|{1}" .format(str (total),str (message.author)))
-    if str(message.content).upper().startswith("*IMPEACH|"):
-        if message.author.id==CREATOR_ID:
-            total= int(str(message.content).split('|')[1])
-            for i in range (total):
-                await client.send_message(message.channel, "!Impeachrequest|eat my shorts")
-            await client.send_message(message.channel, "Impeach spam complete")
+
+
+            
     if str(message.content).upper().startswith("*KDCOMP|"):
         username=str(message.content).split('|')[1]
         url="https://destinytracker.com/d2/profile/pc/{0}".format(username.replace('#','-'))
@@ -199,9 +202,7 @@ async def on_message(message):
         em = discord.Embed(colur=3447003,description="{0} {1}, {2}\n{3}:{4} {5}".format(Months[now.month],now.day,now.year,str(hour),str(now.minute),AMPM))
         await client.send_message(message.channel, embed=em)
     #Pause/Resume
-    if str(message.content).upper()
-    
-    ==("*PAUSE"):
+    if str(message.content).upper()==("*PAUSE"):
         Player.pause()
         em = discord.Embed(colour=3447003)
         em.set_author(name="Music is Paused.")
@@ -256,23 +257,38 @@ async def on_message(message):
                 await client.send_message(message.channel, embed=em)
         except IndexError:
             await client.send_message(message.channel, ("Could not find '"+music4+"' on YouTube."))                                                                      
-    #Command List
+
+
+    
     if str(message.content).upper()=='*COMMANDS':
         em = discord.Embed(title='Hals Commands',colour=3447003)
         em=discord.Embed(title="Command List",description="*Add - Will Send an Invite Link to the channel so you can add him to your server.\n\
         *Code - Will send a link to Github for Hals source Code.\n\
-        *CursedImage - Will send a Cursed Image.\n\
         *Block| - Will Block the stated user from messaging any channel *FOR CREATOR USE ONLY*.\n\
         *UnBlock| - Will UnBlock the stated user to allow them to message any channel *FOR CREATOR USE ONLY*.\n\
         *Block|All - Will block all users in the server that the  message is sent in *FOR CREATOR USE ONLY*.\n\
         *UnBlock|All - Will UnBlock all users in the server that the message is sent in *FOR CREATOR USE ONLY*.\n\
-        *Ksp - Will send a random KSP screenshot from my game libary.\n\
+        *Restart - Will restart Hal's program to clear any erros. *FOR CREATOR USE ONLY* \n\
+        *Meeting | #of voice channels wanted - Will create voice channels | WILL ONLY WORK IF PERMS ARE GIVEN.*FOR CREATOR USE ONLY* \n\
+        \n\
         *Clock - Will display the date and time.\n\
         *play| - Will play any music you want, link or title name will work.\n\
         *Pause - Will pause the current music.\n\
-        *Replay - Will replay the last song. \n\
+        *Repeat - Will replay the last song. \n\
+        *Steam - Will display steam profile link. \n\
+        *Thronemap- Will display Shattered Throne first encounter symbol map. \n\
+        *Test - Will test Hal's Commands in the background to see if he is fully online. \n\
+        *KDOverall| Username - Will display the accounts OVERALL PVP KD. \n\
+        *KDQP | Username - Will display the accounts Quickplay KD. \n\
+        *KDcomp| Username - Will display the accounts COMP PVP KD. \n\
+        *Commands - Display this embed message. \n\
         *Resume - Will resume the current music.")           
         await client.send_message(message.channel, embed=em)
+
+
+
+
+        
     #Work in progress
     #if discord.member.get_user_info = status.offline
      #   server.get_member_named(str(message.content).split('|')[1])
