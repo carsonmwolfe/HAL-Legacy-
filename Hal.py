fe16aba2e9ed2fcd885bc1ebd1572df52b88bc4b
Carson <46657662+Darkarea51@users.noreply.github.com>
2019-10-07 11:15:26 -0700

    Update Hal.py

--- Hal.py
+++ Hal.py
@@ -1,7 +1,6 @@
 #Created November 2018
 #upadted july 8,2019
 #Things To Add/Fix
-#Replay, multiple server music, Destiny API (stats,gear,weekly..), Block all, Fix restart, fix add voice/text, fix change nicknames, 
 import csv
 import discord
 #import asyncio
@@ -23,7 +22,7 @@ Meeting_Room=None
 client=discord.Client()
 #photos(1)=["/home/pi/Desktop/20190119163521_1.JPG","/home/pi/Desktop/20190119162904_1.jpg","/home/pi/Desktop/20190119153640_1.jpg","/home/pi/Desktop/20190119163119_1.jpg","/home/pi/Desktop/20190119162922_1.jpg","/home/pi/Desktop/2019011918210350_1.jpg","/home/pi/Desktop/20190119163119_1.jpg","/home/pi/Desktop/20190119162640_1.jpg","/home/pi/Desktop/20190119161440_1.jpg","/home/pi/Desktop/2019011811719_1.jpg","/home/pi/Desktop/20190119163114_1.jpg","/home/pi/Desktop/20190119143642_1.jpg","/home/pi/Desktop/20170507152646_1.jpg","/home/pi/Desktop/20190120171108_1.jpg","/home/pi/Desktop/20190119162035_1.jpg","/home/pi/Desktop/20190119133028_1.jpg","/home/pi/Desktop/20190119163803_1.jpg","/home/pi/Desktop/20190119162823_1.jpg","/home/pi/Desktop/20190119155705_1.jpg"]     
 commands = []
-command = []                            
+command = []                         
 Player=None
 Memberinfo=[]
 Blocked=[]
@@ -73,7 +72,7 @@ async def on_server_join(server):
 async def on_join():
     for role in server.roles:
         if str(role) == "Swarm":
-            await client.add_roles(Swarm,role)     
+            await client.add_roles(Swarm,role)   
 @client.event
 async def on_message(message):
     global Player
@@ -118,13 +117,13 @@ async def on_message(message):
                     await client.send_message(await client.get_user_info(CREATOR_ID),msg)
                     msg=""
                 if len(str(row))>2 and str(row).startswith("['http")==False:
-                    msg=msg+str(row[0])                                                               
+                    msg=msg+str(row[0])                                               
   #  if str(message.content).upper().startswith("*MINE|"):
   #      if message.author.id in ALLOWED_ID:
   #          total= int(str(message.content).split('|')[1])
   #          for i in range (total):
   #              await client.send_message(message.channel, "!Mine")
-  #          await client.send_message(message.channel, "!Transfer|{0}|{1}" .format(str (total),str (message.author)))         
+  #          await client.send_message(message.channel, "!Transfer|{0}|{1}" .format(str (total),str (message.author)))       
     if str(message.content).upper().startswith("*KDCOMP|"):
         username=str(message.content).split('|')[1]
         url="https://destinytracker.com/d2/profile/pc/{0}".format(username.replace('#','-'))
@@ -145,8 +144,8 @@ async def on_message(message):
         headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         req=str(requests.get(url,headers).content)
         KD=req[60590:].split('"},{"label":"KDA"')[0].split('"displayValue":"')[1]
-        await client.send_message(message.channel,"PVP Quickplay KD:{0}".format(KD)) 
-    #Block/Unblock Feature 
+        await client.send_message(message.channel,"PVP Quickplay KD:{0}".format(KD))
+    #Block/Unblock Feature
     if str(message.content).upper().startswith("*BLOCK|"):
         if message.author.id==CREATOR_ID:
             Blocked.append(message.server.get_member_named(str(message.content).split('|')[1]))
@@ -155,13 +154,13 @@ async def on_message(message):
             await client.send_message(message.channel, embed=em)
         else:
             em = discord.Embed(colour=3447003)
-            em.set_author(name="This is a Admin Only command.")                                          
+            em.set_author(name="This is a Admin Only command.")                                       
     if str(message.content).upper().startswith("*UNBLOCK|"):
         Blocked.remove(message.server.get_member_named(str(message.content).split('|')[1]))
         em = discord.Embed(colour=3447003)
         em.set_author(name="{0} Has Been Unblocked.".format(str(message.server.get_member_named(str(message.content).split('|')[1]))))
         await client.send_message(message.channel, embed=em)
-    #Random Photos  
+    #Random Photos
     if str(message.content).upper()=="*KSP":
         randphoto= photos[random.randrange(0,len(photos))]
         em = discord.Embed(colour=3447003)
@@ -183,7 +182,7 @@ async def on_message(message):
         Player.pause()
         em = discord.Embed(colour=3447003)
         em.set_author(name="Music is Paused.")
-        await client.send_message(message.channel, embed=em) 
+        await client.send_message(message.channel, embed=em)
     if str(message.content).upper()==("*RESUME"):
         Player.resume()
         em = discord.Embed(colour=3447003)
@@ -196,7 +195,7 @@ async def on_message(message):
         em.set_author(name="Music Volume has been changed to {0}".format(str(total))+"%." )
     #if str(message.content).upper()==("*STATUS")
      #   em = discord.Embed(colour=3447003)
-         #em.set_author(name="")       
+         #em.set_author(name="")   
     #Restart
         await client.send_message(message.channel, embed=em)
     if str(message.content).upper()==("*RESTART"):
@@ -233,7 +232,7 @@ async def on_message(message):
                 em.set_author(name="Now Playing")
                 await client.send_message(message.channel, embed=em)
         except IndexError:
-            await client.send_message(message.channel, ("Could not find '"+music4+"' on YouTube."))                                                                      
+            await client.send_message(message.channel, ("Could not find '"+music4+"' on YouTube."))                                                                 
     if str(message.content).upper()=='*COMMANDS':
         em = discord.Embed(title='Hals Commands',colour=3447003)
         em=discord.Embed(title="Command List",description="*Add - Will Send an Invite Link to the channel so you can add him to your server.\n\
@@ -256,7 +255,7 @@ async def on_message(message):
         *KDQP | Username - Will display the accounts Quickplay KD. \n\
         *KDcomp| Username - Will display the accounts COMP PVP KD. \n\
         *Commands - Display this embed message. \n\
-        *Resume - Will resume the current music.")           
+        *Resume - Will resume the current music.")
         await client.send_message(message.channel, embed=em)
     #Work in progress
     #if discord.member.get_user_info = status.offline
@@ -264,7 +263,7 @@ async def on_message(message):
     #    client.change_nickname(message.content.replace('IN':str(time.status.offline))
     #if discord.member.get_user_info = status.online
     #    server.get_member_named(str('nickname'):
-    #Youtube_DL Music System                                       
+    #Youtube_DL Music System                                    
     if str(message.content).upper().startswith("*PLAY|"):
         if Player!=None:
             if Player.is_playing():
@@ -303,12 +302,12 @@ async def on_message(message):
         Meeting_Room=await client.create_channel(message.server,"Meeting Room",type=discord.ChannelType.voice)
         await client.edit_channel(Meeting_Room,user_limit=int(str(message.content).split("|")[1]))
         Meeting_Room=Meeting_Room.id
-#@client.event       
+#@client.event  
 #async def on_voice_state_update(before,after):
 #    global Meeting_Room
 #    if before.voice_channel==after.server.get_channel(Meeting_Room):
 #        Meeting_Room=after.server.get_channel(Meeting_Room)
 #        print(Meeting_Room.voice_members)
 #        if len(Meeting_Room.voice_members)==0:
-#            await client.delete_channel(Meeting_Room) 
+#            await client.delete_channel(Meeting_Room)
 client.loop.run_until_complete(client.start(TokenDoc.token))
